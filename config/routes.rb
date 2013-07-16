a240b710835af88b16012cd81276e9f19bd8ea89
James Page <jpage@reckloose.com>
2013-07-16 19:39:39 -0400

    added shirt images and implemented search

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Shirtz::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+resources :shirts
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
